8477a25a874488fee9fadb58488fcf6db3977307
Дмитрий <dima-dimka04@yandex.ru>
2023-12-16 19:40:10 +0300

    include file update

--- include/head.hpp
+++ include/head.hpp
@@ -1,29 +1,20 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <vector>
+
 using namespace std;
 
-const int LEN = 301;
+
+enum class Priority {scobe, num, plus, mult, unar};
+enum LexemeType {lsc, rsc, num, binary, unary, unknown};
 
 class Lexeme
 {
-private:
-	char infix[LEN]; //infix write
-	char postfix[LEN]; //postfix write
-	stack<double>num; //stack for numbers
-	stack<char>op; //stack for operators
-
-	int priority(char symb); //priority of symbols and operations
-	bool isOperator(char symb); //checks is symbol math operator
-	bool isNumber(char symb); //checks is symbol number
-
 public:
-	Lexeme() {}
-	~Lexeme() {}
-	bool checkSc(char* s); // checks for right scarb order
-	void delStack(); //delete all in stack
-	void infToPost(); //infix write to postfix one
-	double Counter(); //calculator
-	double postCounter(); //postfix calculator
-	int statusPath();
+	Priority priority;
+	LexemeType type;
+	string value;
 };
+
+enum class Status {StartStatus, NumberStatus, LScobeStatus, RScobeStatus, UnaryStatus, BinaryStatus, ErrorStatus, EndStatus};
